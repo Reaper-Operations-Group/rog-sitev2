3e53ffdea5989b81650e942fbdbba24ad255eb63
Kieran Jaggernauth <thejugg63@gmail.com>
2024-03-22 17:09:43 -0500

    Added MUI Component Library

---
+++ src/app/member/profile/page.tsx
@@ -0,0 +1,9 @@
+import { Typography } from "@mui/material";
+
+export default function Profile(){
+    return (
+        <Typography variant="h1">
+            Profile Page
+        </Typography>
+    );
+}
