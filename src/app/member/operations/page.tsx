91bf6f9bcd532b63d8417767df4511613336e835
Kieran Jaggernauth <thejugg63@gmail.com>
2024-03-22 23:57:02 -0500

    Added Logi & Ops page

---
+++ src/app/member/operations/page.tsx
@@ -0,0 +1,9 @@
+import { Typography } from "@mui/material";
+
+export default function Operations(){
+    return (
+        <Typography variant="h1">
+            Operations Page
+        </Typography>
+    );
+}
